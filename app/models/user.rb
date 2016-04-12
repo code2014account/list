587f63c4a03c146853071ee9939a0ff0630b4371
Matt <matt24ray@gmail.com>
2016-04-12 18:02:01 -0500

    profile2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+         def avatar_url(size)
+            gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+            "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+          end
 end

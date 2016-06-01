84844986f7f560006609c4df89c361f2ed2289c2
Matt <matt24ray@gmail.com>
2016-05-31 23:46:44 -0500

    yes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :items, only: [:create, :update, :destroy]
   end
 
-  root 'welcome#index'
+  root 'users#show'
  # did not work
  # get '/:id' => 'controller#action'
 
